5f8a9dc6271ccfbc50bd8d0f164ef7af74e9f051
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-10-30 00:41:17 -0400

    check perf

--- src/parse.jl
+++ src/parse.jl
@@ -1,18 +1,22 @@
+# Compute the narrowest unsigned integer type to represent binary number encoded in a string
+_min_uint_type(s::AbstractString) = min_uint_type(ncodeunits(s))
+_min_uint_type(v::AbstractVector{UInt8}) = min_uint_type(length(v))
+
 """
-    parse_bin(s::AbstractString)
+    parse_bin([::Type{UIntT}], s::AbstractString)
 
 Convert an integer coded as a binary string to an unsigned integer.
-The return type is the narrowest suitable unsigned integer type .
+The return type is `UIntT`. If the first argument is omitted, then
+the return type is the narrowest suitable unsigned integer type .
 """
-parse_bin(s::AbstractString) = parse_bin(min_uint_type(ncodeunits(s)), s)
-parse_bin(c::Base.CodeUnits) = parse_bin(min_uint_type(length(c)), c)
+parse_bin(s) = parse_bin(_min_uint_type(s), s)
 parse_bin(::Type{T}, s::AbstractString) where T = parse_bin(T, codeunits(s))
-parse_bin(::Type{T}, c::Base.CodeUnits) where T = _parse_bin(c, get_masks(T), T)
+parse_bin(::Type{T}, c::Base.CodeUnits) where T = _parse_bin(T, c, get_masks(T))
 
-_parse_bin(s::AbstractString, facs, ::Type{T}) where T = _parse_bin(codeunits(s), facs, T)
+_parse_bin(::Type{T}, s::AbstractString, facs) where T = _parse_bin(T, codeunits(s), facs)
 
 # c - code units, (or Vector{UInt8}, but unsafe_wrap allocates, codeunits does not)
-@inline function _parse_bin(c, facs, ::Type{T})::T where {T}
+@inline function _parse_bin(::Type{T}, c, facs)::T where {T}
     x::T = zero(T)
     length(facs) >= length(c) || throw(BoundsError(facs, length(c))) # This line increases perf.
     @inbounds for i in eachindex(c)
@@ -21,7 +25,7 @@ _parse_bin(s::AbstractString, facs, ::Type{T}) where T = _parse_bin(codeunits(s)
     return x
 end
 
-function _parse_bin(c, ::Type{T})::T where T
+function _parse_bin(::Type{T}, c)::T where T
     x::T = zero(T)
     fac::T = one(T)
     @inbounds for i in eachindex(c)
