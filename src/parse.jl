a5ac83c368a43c8428747ec863a3954396319d70
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-10-29 21:18:12 -0400

    check perf

--- src/parse.jl
+++ src/parse.jl
@@ -1,11 +1,4 @@
-function parse_bin(s::AbstractString)
-    # _byte_width = min_uint_byte_width(ncodeunits(s))
-    # uint_type = uint_type_bytes(_byte_width)
-    uint_type = min_uint_type(ncodeunits(s))
-    return _parse_bin(s, uint_type)
-end
-
-#parse_bin(::Type{T}, s::AbstractString) where T = _parse_bin(s, byte_width(T), T)::T
+parse_bin(s::AbstractString) = _parse_bin(s, min_uint_type(ncodeunits(s)))
 parse_bin(::Type{T}, s::AbstractString) where T = _parse_bin(s, T)::T
 
 function parse_bin(c::Base.CodeUnits)
@@ -15,12 +8,7 @@ function parse_bin(c::Base.CodeUnits)
 end
 
 parse_bin(::Type{T}, c::Base.CodeUnits) where T = _parse_bin(c, T)::T
-#parse_bin(::Type{T}, c::Base.CodeUnits) where T = _parse_bin(c, byte_width(T), T)::T
-
 parse_bin(::Type{T}, s::AbstractString, _byte_width::Integer) where T = _parse_bin(s, _byte_width, T)
-
-# _parse_bin(s::AbstractString, _byte_width::Integer, ::Type{T}) where T =
-#     _parse_bin(codeunits(s), _byte_width, T)
 
 _parse_bin(s::AbstractString, ::Type{T}) where T =
     _parse_bin(codeunits(s), T)
@@ -28,19 +16,19 @@ _parse_bin(s::AbstractString, ::Type{T}) where T =
 _parse_bin(c::Base.CodeUnits, _byte_width, ::Type{T}) where T = __parse_bin(c, get_masks(T), T)
 _parse_bin(c::Base.CodeUnits, ::Type{T}) where T = __parse_bin(c, get_masks(T), T)
 
+__parse_bin(s::AbstractString, facs, ::Type{T}) where {T} = __parse_bin(codeunits(s), facs, T)
+
 # c -- code units, (or Vector{UInt8}, but unsafe_wrap allocates, codeunits does not)
 @inline function __parse_bin(c, facs, ::Type{T})::T where {T}
     x::T = zero(T)
     length(facs) >= length(c) || throw(BoundsError(facs, length(c))) # This line increases perf.
     @inbounds for i in eachindex(c)
-        if c[i]::UInt8 == _ONE_CHAR_CODE
-            x::T += facs[i]::T
-        end
+        c[i]::UInt8 == _ONE_CHAR_CODE && (x::T += facs[i]::T)
     end
     return x
 end
 
-__parse_bin(s::AbstractString, facs, ::Type{T}) where {T} = __parse_bin(codeunits(s), facs, T)
+######
 
 @inline function _parse_bin_general(::Type{T}, s::AbstractString)::T where T
     x::T = zero(T)
