dda2b7d511be67131602af028b14237f4c348b6b
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-10-30 22:57:16 -0400

    Revert to previous commit because better perf

--- src/parse.jl
+++ src/parse.jl
@@ -2,9 +2,6 @@
 _min_uint_type(s::AbstractString) = min_uint_type(ncodeunits(s))
 _min_uint_type(v::AbstractVector{UInt8}) = min_uint_type(length(v))
 
-_length_in_UInt8(s::AbstractString) = ncodeunits(s) # not all AbstractString
-_length_in_UInt8(v::AbstractVector{UInt8}) = length(v)
-
 """
     parse_bin([::Type{UIntT}], s::AbstractString)
 
@@ -13,23 +10,14 @@ The return type is `UIntT`. If the first argument is omitted, then
 the return type is the narrowest suitable unsigned integer type .
 """
 parse_bin(s) = parse_bin(_min_uint_type(s), s)
+parse_bin(::Type{T}, s::AbstractString) where T = parse_bin(T, codeunits(s))::T
+parse_bin(::Type{T}, c::AbstractVector{UInt8}) where T = _parse_bin(T, c, get_masks(T))::T
+#parse_bin(::Type{T}, c::Base.CodeUnits) where T = _parse_bin(T, c, get_masks(T))
 
-_ensure_string(s) = s
-_ensure_string(c::Base.CodeUnits) = c.s
-
-function parse_bin(::Type{T}, s) where T
-    BigInt_better_thresh = 650 # approx.
-    if _length_in_UInt8(s) > BigInt_better_thresh
-        return T(parse(BigInt, _ensure_string(s); base=2))
-    end
-    return parse_bin(T, codeunits(s))::T
-end
-
-parse_bin(::Type{T}, c::AbstractVector{UInt8}) where T = _parse_bin1(T, c, get_masks(T))::T
-_parse_bin1(::Type{T}, s::AbstractString, facs) where T = _parse_bin1(T, codeunits(s), facs)::T
+_parse_bin(::Type{T}, s::AbstractString, facs) where T = _parse_bin(T, codeunits(s), facs)::T
 
 # c - code units, (or Vector{UInt8}, but unsafe_wrap allocates, codeunits does not)
-@inline function _parse_bin1(::Type{T}, c, facs)::T where {T}
+@inline function _parse_bin(::Type{T}, c, facs)::T where {T}
     x::T = zero(T)
     length(facs) >= length(c) || throw(BoundsError(facs, length(c))) # This line increases perf.
     @inbounds for i in eachindex(c)
@@ -38,12 +26,7 @@ _parse_bin1(::Type{T}, s::AbstractString, facs) where T = _parse_bin1(T, codeuni
     return x
 end
 
-# Rarely if ever called.
-function _parse_bin1(::Type{T}, c::Base.CodeUnits, ::Nothing)::T where T
-    return convert(T, parse(BigInt, c.s; base=2))
-end
-
-function _parse_bin_general(::Type{T}, c, ::Nothing)::T where T
+function _parse_bin(::Type{T}, c)::T where T
     x::T = zero(T)
     fac::T = one(T)
     @inbounds for i in eachindex(c)
@@ -53,33 +36,4 @@ function _parse_bin_general(::Type{T}, c, ::Nothing)::T where T
         fac *= 2
     end
     return x
-end
-
-# Ignore all (single code unit) characters except 0 and 1
-@inline function _parse_bin_ignore(::Type{T}, c, facs)::T where {T}
-    x::T = zero(T)
-    length(facs) >= length(c) || throw(BoundsError(facs, length(c))) # This line increases perf.
-    j::Int = 1
-    @inbounds for i in eachindex(c)
-        ci::UInt8 = c[i]::UInt8
-        if ci == _ONE_CHAR_CODE
-            x::T += facs[j]::T
-        elseif ci !=  _ZERO_CHAR_CODE
-            continue
-        end
-        j += 1
-    end
-    return x
-end
-
-count_bits(s::AbstractString) = count_bits(codeunits(s))
-
-function count_bits(v::AbstractVector{UInt8})
-    cnt = 0
-    for x::UInt8 in v
-        if x == _ZERO_CHAR_CODE || x == _ONE_CHAR_CODE
-            cnt += 1
-        end
-    end
-    return cnt
 end
