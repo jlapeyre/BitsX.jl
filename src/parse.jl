7668ef78dc5812e85d30464c21787c7ad9b4d7c9
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-10-31 16:35:45 -0400

    Fix up filtering option

--- src/parse.jl
+++ src/parse.jl
@@ -8,7 +8,7 @@ _length_in_UInt8(s::AbstractString) = ncodeunits(s) # not all AbstractString
 _length_in_UInt8(v::AbstractVector{UInt8}) = length(v)
 
 """
-    parse_bin([::Type{UIntT}], s::Union{AbstractString, AbstractVector{UInt8}})
+    parse_bin([::Type{UIntT}], s::Union{AbstractString, AbstractVector{UInt8}}; filter=false)
 
 Convert an integer coded as a binary string (characters `'1'` and `'0'`) to an unsigned integer.
 The return type is `UIntT`. If the first argument is omitted, then
@@ -21,56 +21,67 @@ a type that has not been pre-generated will be created.
 Usually `parse_bin` is faster than `Base.parse`. The factor varies from about 5 to 9. However, for bit widths
 larger than about 650, `parse_bin` first parses the string as a `BigInt` using `Base.parse` and then converts
 to the appropriate fixed-wdith unsigned integer type.
-"""
-parse_bin(s; filter::Bool=false) = parse_bin(_min_uint_type(s), s)
 
-function parse_bin(::Type{T}, s_or_c::Union{AbstractString, CodeUnits}) where T # ; filter::Bool=false) where T
+If `filter` is `true`, then characters other than `'1'` and `'0'` are ignored rather than raising an error. In
+this way, formatting, such as spaces, may be included in the input string.
+"""
+parse_bin(s; filter::Bool=false) = parse_bin(_min_uint_type(s), s; filter=filter)
+
+function parse_bin(::Type{T}, s_or_c::Union{AbstractString, CodeUnits}; filter::Bool=false) where T # ; filter::Bool=false) where T
     BigInt_better_thresh = 650 # approx.
     if _length_in_UInt8(s_or_c) > BigInt_better_thresh
         return T(parse(BigInt, s_or_c; base=2)) # TODO: do filtering if requested
     end
-    __parse_bin(T, s_or_c)::T
+    _parse_bin(T, s_or_c, filter)::T
 end
 
 # If c is not CodeUnits, we don't know how to recover string, so can't use BigInt
-parse_bin(::Type{T}, c::AbstractVector{UInt8}) where T = __parse_bin(T, c)
+parse_bin(::Type{T}, c::AbstractVector{UInt8}; filter::Bool=false) where T = _parse_bin(T, c, filter)
 
-__parse_bin(::Type{T}, s::AbstractString) where T = __parse_bin(T, codeunits(s))
-__parse_bin(::Type{T}, c::AbstractVector{UInt8}) where T = _parse_bin(T, c, get_one_bit_masks(T))::T
-#__parse_bin(::Type{T}, c::AbstractVector{UInt8}) where T = _parse_bin_permissive(T, c, get_one_bit_masks(T))::T
+_parse_bin(::Type{T}, s::AbstractString, filter) where T = _parse_bin(T, codeunits(s), filter)
+
+function _parse_bin(::Type{T}, c::AbstractVector{UInt8}, filter::Bool) where T
+    return filter ? __parse_bin_permissive(T, c)::T :
+        __parse_bin(T, c, get_one_bit_masks(T))::T
+end
+#_parse_bin(::Type{T}, c::AbstractVector{UInt8}) where T = __parse_bin_permissive(T, c, get_one_bit_masks(T))::T
 
 _parse_via_BigInt(::Type{T}, c::CodeUnits) where T = _parse_via_BigInt(T, c.s)
 _parse_via_BigInt(::Type{T}, s::AbstractString) where T = T(parse(BigInt, _ensure_string(s_or_c); base=2))
 
-
 # c - code units, (or Vector{UInt8}, but unsafe_wrap allocates, codeunits does not)
-@inline function _parse_bin(::Type{T}, c, one_bit_masks)::T where {T}
+@inline function __parse_bin(::Type{T}, c, one_bit_masks)::T where {T}
     x::T = zero(T)
     length(one_bit_masks) >= length(c) || throw(BoundsError(one_bit_masks, length(c))) # This line increases perf.
     @inbounds for i in eachindex(c)
-        c[i]::UInt8 == _ONE_CHAR_CODE && (x::T += one_bit_masks[i]::T)
-    end
-    return x
-end
-
-@inline function _parse_bin_permissive(::Type{T}, c, one_bit_masks)::T where {T}
-    x::T = zero(T)
-    #    length(one_bit_masks) >= length(c) || throw(BoundsError(one_bit_masks, length(c))) # This line increases perf.
-    # Need some kind of check here
-    j::Int = 1
-    @inbounds for i in eachindex(c)
-        if c[i]::UInt8 == _ONE_CHAR_CODE
-            x::T += one_bit_masks[j]::T
-            j += 1
-        elseif c[i]::UInt8 == _ZERO_CHAR_CODE
-            j += 1
+        ch::UInt8 = c[i]
+        if ch == _ONE_CHAR_CODE
+            x::T += one_bit_masks[i]::T
+        elseif ch != _ZERO_CHAR_CODE
+            throw(DomainError("Invalid byte in binary string ", ch))
         end
     end
     return x
 end
 
-_parse_bin(::Type{T}, c, ::Nothing) where T = _parse_bin(T, c) # get_masks returns nothing if masks not available
-function _parse_bin(::Type{T}, c)::T where T
+
+__parse_bin_permissive(::Type{T}, c, ::Nothing) where T = __parse_bin(T, c) # get_masks returns nothing if masks not available
+function __parse_bin_permissive(::Type{T}, c)::T where T
+    x::T = zero(T)
+    one_bit_mask::T = one(T)
+    @inbounds for i in eachindex(c)
+        if c[i]::UInt8 == _ONE_CHAR_CODE
+            x += one_bit_mask
+            one_bit_mask *= 2
+        elseif c[i]::UInt8 == _ZERO_CHAR_CODE
+            one_bit_mask *= 2
+        end
+    end
+    return x
+end
+
+__parse_bin(::Type{T}, c, ::Nothing) where T = __parse_bin(T, c) # get_masks returns nothing if masks not available
+function __parse_bin(::Type{T}, c)::T where T
     x::T = zero(T)
     one_bit_mask::T = one(T)
     @inbounds for i in eachindex(c)
@@ -82,20 +93,22 @@ function _parse_bin(::Type{T}, c)::T where T
     return x
 end
 
-_parse_bin_permissive(::Type{T}, c, ::Nothing) where T = _parse_bin(T, c) # get_masks returns nothing if masks not available
-function _parse_bin_permissive(::Type{T}, c)::T where T
-    x::T = zero(T)
-    one_bit_mask::T = one(T)
-    @inbounds for i in eachindex(c)
-        if c[i]::UInt8 == _ONE_CHAR_CODE
-            x += one_bit_mask
-            one_bit_mask *= 2
-        elseif c[i]::UInt8 == _ZERO_CHAR_CODE
-            one_bit_mask *= 2
-        end
-    end
-    return x
-end
+## Don't use this. easy user bad input may cause segfault
+# @inline function __parse_bin_permissive(::Type{T}, c, one_bit_masks)::T where {T}
+#     x::T = zero(T)
+#     #    length(one_bit_masks) >= length(c) || throw(BoundsError(one_bit_masks, length(c))) # This line increases perf.
+#     # Need some kind of check here
+#     j::Int = 1
+#     @inbounds for i in eachindex(c)
+#         if c[i]::UInt8 == _ONE_CHAR_CODE
+#             x::T += one_bit_masks[j]::T
+#             j += 1
+#         elseif c[i]::UInt8 == _ZERO_CHAR_CODE
+#             j += 1
+#         end
+#     end
+#     return x
+# end
 
 """
     count_bits(s::AbstractString)
